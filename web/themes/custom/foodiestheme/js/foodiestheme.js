1aa35d9d70dec46afab93b5946ca58bd1df992ea
Darian Alexis Ortiz <dalexiso@gmail.com>
2022-08-15 13:11:46 +0000

    Config and composer added

---
+++ web/themes/custom/foodiestheme/js/foodiestheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Foodies behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.foodiestheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
---
+++ web/themes/custom/foodiestheme/theme-settings.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Theme settings form for Foodies theme.
+ */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function foodiestheme_form_system_theme_settings_alter(&$form, &$form_state) {
+
+  $form['foodiestheme'] = [
+    '#type' => 'details',
+    '#title' => t('Foodies'),
+    '#open' => TRUE,
+  ];
+
+  $form['foodiestheme']['font_size'] = [
+    '#type' => 'number',
+    '#title' => t('Font size'),
+    '#min' => 12,
+    '#max' => 18,
+    '#default_value' => theme_get_setting('font_size'),
+  ];
+
+}
